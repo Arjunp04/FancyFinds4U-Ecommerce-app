6aa0cc5ef4bf4b978f05cf16b70c042ce1b06e63
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2025-01-07 16:00:37 +0530

    add to cart, get cart count functioanlity done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShopContext } from "../context/Shopcontext.jsx";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const { setShowSearch } = useContext(ShopContext);
+  const { setShowSearch , getCartCount} = useContext(ShopContext);
 
   return (
     <div className="flex justify-between items-center font-medium py-5">
@@ -56,7 +56,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart icon" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white rounded-full text-[8px] aspect-square">
-            10
+            {getCartCount()}
           </p>
         </Link>
 
--- frontend/src/context/Shopcontext.jsx
+++ frontend/src/context/Shopcontext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { toast, Zoom } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -8,11 +9,65 @@ const ShopContextProvider = (props) => {
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false); //display the search bar
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error(" Select Product Size", {
+        position: "top-center",
+        autoClose: 1500,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Zoom,
+      });
+      return;
+    }
+
+    // console.log(itemId, size);
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    setCartItems(cartData);
+  };
+
+  const getCartCount =  () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items])
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {}
+    }
+    return totalCount;
+  };
 
   const value = {
     products,
     currency,
-    delivery_fee,search,setSearch,setShowSearch,showSearch
+    delivery_fee,
+    search,
+    setSearch,
+    setShowSearch,
+    showSearch,
+    cartItems,
+    setCartItems,
+    addToCart,
+    getCartCount,
   };
 
   return (
