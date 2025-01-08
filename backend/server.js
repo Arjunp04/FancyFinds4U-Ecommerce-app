dd0b3d894d3ca9acb3252d2f44e0cd293be12ed6
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2025-01-08 19:11:40 +0530

    database and cloudinary config setup done

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 import express from "express";
 import "dotenv/config";
-import cors from "cors"
+import cors from "cors";
+import connectToDB from "./config/connectDB.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 // ------------------- app config ------------------ //
 
-//CREATING INSTANCE OF EXPPRESS PACKAGE 
+//CREATING INSTANCE OF EXPPRESS PACKAGE
 const app = express();
 const port = process.env.PORT || 4000;
-
+connectToDB();
+connectCloudinary();
 
 // ---------------------- middlewares --------------------//
 app.use(express.json());
 app.use(cors());
 
-//----------------  api endpoints --------------------// 
+//----------------  api endpoints --------------------//
 
-app.get("/", (req,res) => {
-    res.json("welcome to server");
-})
-
+app.get("/", (req, res) => {
+  res.json("welcome to server");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on ${`http://localhost:${port}`}`)
-})
+  console.log(`Server running on ${`http://localhost:${port}`}`);
+});
