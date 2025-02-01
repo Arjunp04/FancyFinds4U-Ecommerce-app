3e624b4de2bef0859319c9e6887c6bc2d5db7f5f
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2025-02-01 16:02:55 +0530

    order model, routes and placeorder using cod api created

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,98 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
+// ********* placing order using cod ************** //
+
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, amount, items, address } = req.body;
+    console.log(req.body);
+
+    const orderData = {
+      userId,
+      amount,
+      items,
+      address,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    //clearing the cart data once order is placed
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// ********* placing order using stripe ************** //
+
+const placeOrderStripe = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// ********* placing order using razorpay ************** //
+
+const placeOrderRazorPay = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// ******** all orders data for admin panel ********** //
+
+const allOrders = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// ******** user orders data for frontend ********** //
+
+const userOrders = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const orders = await orderModel.find({ userId });
+
+    res.json({
+      success: true,
+      message: "User orders data fetched successfully",
+      orders,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// ******** update orders status from admin panel ********** //
+
+const updateOrderStatus = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export {
+  placeOrder,
+  placeOrderRazorPay,
+  placeOrderStripe,
+  allOrders,
+  userOrders,
+  updateOrderStatus,
+};
---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  placeOrder,
+  placeOrderRazorPay,
+  placeOrderStripe,
+  allOrders,
+  userOrders,
+  updateOrderStatus,
+} from "../controllers/orderController.js";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/authUser.js";
+
+const orderRouter = express.Router();
+
+//admin features
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateOrderStatus);
+
+//payment features
+orderRouter.post("/place-order", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, placeOrderRazorPay);
+
+//user features
+orderRouter.get("/user-orders", authUser, userOrders);
+
+export default orderRouter;
