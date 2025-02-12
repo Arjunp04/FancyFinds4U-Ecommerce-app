4bc4f39eacb231ecb3801c056e9d2a39083e6961
Arjun Ramesh Prajapati <arjunprajapati4102001@gmail.com>
2025-02-12 23:02:51 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -17,48 +17,21 @@ const port = process.env.PORT || 4000;
 connectToDB();
 connectCloudinary();
 
-// ---------------------- CORS Handling -------------------- //
-const allowedOrigins = [
-  process.env.FRONTEND_URL,
-  process.env.ADMIN_FRONTEND_URL,
-];
-
-// Dynamic CORS Middleware
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization","Origin"],
-};
-
-app.use(cors(corsOptions));
-
-// Manually set CORS Headers for all requests
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
-
 // ---------------- Middleware ---------------- //
 app.use(express.json());
+
+// ---------------------- CORS Handling -------------------- //
+const corOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://fancyfinds4u.vercel.app",
+    "https://fancyfinds4u-admin-panel.vercel.app/",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corOptions));
 
 // ---------------- Routes ---------------- //
 app.use("/api/user", userRouter);
